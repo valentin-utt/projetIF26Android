45cbedcadc887d2e00967907018b8e8f6ba3c234
val-utt <valentin.guilloux@utt.fr>
2018-10-01 07:19:03 +0200

    Création d'une interface basique et intégration de l'API Google maps

--- app/src/main/java/fr/utt/if26/projet/MainActivity.java
+++ app/src/main/java/fr/utt/if26/projet/MainActivity.java
@@ -1,13 +1,44 @@
 package fr.utt.if26.projet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mapButton;
+    private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get UI elements
+
+        mapButton = findViewById(R.id.mapButton);
+        signInButton = findViewById(R.id.signInButton);
+
+        //set onClickEvents
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), MapsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/fr/utt/if26/projet/MapsActivity.java
@@ -0,0 +1,51 @@
+package fr.utt.if26.projet;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(
+                this, R.raw.style_json));
+
+        // Add a marker to utt and move the camera
+        LatLng uttPosition = new LatLng(48.269126, 4.066899);
+        MarkerOptions uttMarker = new MarkerOptions().position(uttPosition).title("Cachette de l'UTT");
+        mMap.addMarker(uttMarker);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(uttPosition));
+    }
+}
